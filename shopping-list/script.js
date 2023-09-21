07ba418307fb20ba42845f362a881edfadfcf865
VANESSA MARQUES <vanessa.m.a1989@gmail.com>
2023-09-21 08:47:44 +0200

    selected ul from html, created function to create li el, passed text input to create el function on submit, created li el, appended li to ul

--- shopping-list/script.js
+++ shopping-list/script.js
@@ -1,10 +1,21 @@
 const form = document.querySelector('form');
 const input = document.getElementById('item-input');
+const unorderedList = document.querySelector('ul');
 
 const handleFormSubmit = (e) => {
   e.preventDefault();
-  console.log(input.value);
+  const newItem = input.value;
+  createNewItem(newItem);
   form.reset();
+};
+
+const createNewItem = (newItem) => {
+  const li = document.createElement('li');
+  li.textContent = newItem;
+  //or
+  //li.appendChild(document.createTextNode(newItem));
+
+  unorderedList.appendChild(li);
 };
 
 form.addEventListener('submit', handleFormSubmit);
