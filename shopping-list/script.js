425af656735fc8f186a35ae2788d40a4cd3d27a7
VANESSA MARQUES <vanessa.m.a1989@gmail.com>
2023-09-21 09:18:41 +0200

    created createBtn and createBtn functions; each creates its respective element with classes. Appended icon to button and appended button to li via function calling

--- shopping-list/script.js
+++ shopping-list/script.js
@@ -9,9 +9,27 @@ const handleFormSubmit = (e) => {
   form.reset();
 };
 
+const createIcon = () => {
+  const icon = document.createElement('i');
+  icon.classList.add('fa-solid', 'fa-xmark');
+  return icon;
+};
+
+const createBtn = () => {
+  const button = document.createElement('button');
+  button.classList.add('btn-link', 'remove-item', 'text-red');
+  //you could also pass icon classes in the function below as argument
+  const icon = createIcon();
+  button.appendChild(icon);
+  return button;
+};
+
 const createNewItem = (newItem) => {
   const li = document.createElement('li');
   li.textContent = newItem;
+  //you could also pass button classes in the function below as argument
+  const button = createBtn();
+  li.appendChild(button);
   //or
   //li.appendChild(document.createTextNode(newItem));
 
