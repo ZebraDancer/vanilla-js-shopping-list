46cac692b96f3ef75c478b7f885bc4a874366570
VANESSA MARQUES <vanessa.m.a1989@gmail.com>
2023-09-21 15:56:10 +0200

    create state function to clear and add css classses based on li length and added prompt to delete item event

--- shopping-list/script.js
+++ shopping-list/script.js
@@ -1,6 +1,8 @@
 const form = document.querySelector('form');
 const input = document.getElementById('item-input');
 const unorderedList = document.querySelector('ul');
+const clearAllBtn = document.getElementById('clear');
+const filterInput = document.getElementById('filter');
 
 const handleFormSubmit = (e) => {
   e.preventDefault();
@@ -30,20 +32,63 @@ const createNewItem = (newItem) => {
   //you could also pass button classes in the function below as argument
   const button = createBtn();
   li.appendChild(button);
+  localStorage.setItem('new item', li);
   //or
   //li.appendChild(document.createTextNode(newItem));
 
   unorderedList.appendChild(li);
+  resetState();
 };
 
 const removeItem = (e) => {
   //you want to target the i element, not the whole ul or li
   //its parent is the button and in order to grab that from the DOM, you need to target the parent element of i - button - and you do so by targetting the class of "remove-item"
+
   if (e.target.parentElement.classList.contains('remove-item')) {
-    //you need to go two levels up in order to target the actual li to be deleted, hence the double parentElement
-    e.target.parentElement.parentElement.remove();
+    if (confirm('Are you sure?'))
+      //you need to go two levels up in order to target the actual li to be deleted, hence the double parentElement
+      e.target.parentElement.parentElement.remove();
+    resetState();
   }
 };
 
+const removeAllItems = () => {
+  const listItems = Array.from(unorderedList.children);
+
+  listItems.forEach((item) => {
+    item.remove();
+  });
+  resetState();
+  //or
+  //  listItem.innerHTML = ''
+
+  //  while(listItem.firstChild){
+  //  listItem.removeChild(listItem.firstChild)
+  //  }
+};
+
+const resetState = () => {
+  const items = document.querySelectorAll('li');
+
+  // if (items.length === 0) {
+  //   clearAllBtn.style.display = 'none';
+  //   filterInput.style.display = 'none';
+  // } else {
+  //   filterInput.style.display = 'block';
+  //   clearAllBtn.style.display = 'block';
+  // }
+
+  if (items.length === 0) {
+    clearAllBtn.classList.add('hide-btn');
+    filterInput.classList.add('hide-filter');
+  } else {
+    clearAllBtn.classList.remove('hide-btn');
+    filterInput.classList.remove('hide-filter');
+  }
+};
+
+clearAllBtn.addEventListener('click', removeAllItems);
 unorderedList.addEventListener('click', removeItem);
 form.addEventListener('submit', handleFormSubmit);
+
+resetState();
